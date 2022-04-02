176b17a9f210ce6628fabab863064dccc270c0b2
Luke Simmons <luke5083@live.com>
2022-04-02 00:41:51 -0500

    Encrypt and decrypt text

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import py7zr
+from os import urandom
+from Crypto.Cipher import AES
 import os
 
 # Get home directory
 home_path = os.path.expanduser('~')
-print(home_path)
 home_path = "/home/luke/Downloads/HackathonSpring2022/Ransomwarer/test/"
 
-with py7zr.SevenZipFile('allUrFilesL0L.7z', 'w', password='HotzFellas') as archive:
-    archive.writeall(home_path)
+# For Generating cipher text
+secret_key = b"HotzFellas000000"
+iv = b"HotzFellas000000"
+obj = AES.new(secret_key, AES.MODE_CBC, iv)
+
+# Encrypt the message
+message = 'Lorem Ipsum text'
+message_bytes = message.encode('utf-8')
+print('Original message is: ', message_bytes.decode('utf-8'))
+encrypted_text = obj.encrypt(message_bytes)
+print('The encrypted text', encrypted_text)
+
+# Decrypt the message
+rev_obj = AES.new(secret_key, AES.MODE_CBC, iv)
+decrypted_text = rev_obj.decrypt(encrypted_text)
+print('The decrypted text', decrypted_text.decode('utf-8'))
